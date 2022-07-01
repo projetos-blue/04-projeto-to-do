809de6fc7c1d1af7340de04986395dd369a6a5bc
rafmob <89050871+rafmob@users.noreply.github.com>
2022-06-30 23:14:29 -0300

    inicio do projeto, cabeçalho

---
+++ src/header/header.jsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+function Header() {
+  return (
+    <section className='header'>
+        <div>
+            <h2>Minha Lista de Tarefas</h2>
+        </div>
+    </section>
+  );
+}
+
+export default Header;
